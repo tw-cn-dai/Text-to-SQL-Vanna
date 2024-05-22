fb3d46277891bc3b299e38a97aa97bd6620f3311
weiwang42210 <wei.wang3@thoughtworks.com>
2024-05-22 14:34:25 +0800

    upload code

--- README.md
+++ README.md
@@ -1,2 +1,77 @@
-# Text-to-SQL-Vanna
-Text to SQL generation via Vanna
+# text2sql-vanna
+Text to SQL generation via Vanna.   
+
+This repo works in two easy steps - train a RAG "model" on your data, and then ask questions which will return SQL queries that can be set up to automatically run on your database.
+
+Please refer to  https://github.com/vanna-ai/vanna  for information related to Vanna.
+
+
+# **Getting started**
+
+# **Install**
+    pip install -r requirements.txt
+    cd src/vanna 
+    pip install -e .
+
+# **Import**
+
+class MyVanna(ChromaDB_VectorStore, OpenAI_Chat):
+    def __init__(self, config=None):
+        ChromaDB_VectorStore.__init__(self, config=config)
+        OpenAI_Chat.__init__(self, config=config)
+        
+vn = MyVanna(config={
+                'base_url':"http://bj.private.gluon-meson.tech:11000/model-proxy/v1", 
+                'api_key': 'sk-...', 
+                'model': 'gpt-4-...',
+                'path': dir_to_save_vector_db
+                        }
+                )
+# **Training**
+## **Train with DDL Statements**
+DDL statements contain information about the table names, columns, data types, and relationships in your database.
+
+vn.train(ddl="""
+        CREATE TABLE IF NOT EXISTS my-table (
+            id INT PRIMARY KEY,
+            name VARCHAR(100),
+            age INT
+            )
+     """)
+## **Train with Documentation**
+Sometimes you may want to add documentation about your business terminology or definitions.
+
+vn.train(documentation="Our business defines XYZ as ...")
+
+## **Train with SQL**
+
+vn.train(sql="SELECT name, age FROM my-table WHERE name = 'John Doe'")
+
+## **Train with SQL-Question pairs**
+vn.train(
+        question="Name movie titles released in year 1945. Sort the listing by the descending order of movie popularity",
+         sql="SELECT movie_title FROM movies WHERE movie_release_year = 1945 ORDER BY movie_popularity DESC LIMIT 1"
+         )
+## **Train plan**
+The information schema query may need some tweaking depending on your database. This is a good starting point.
+
+df_information_schema = vn.run_sql("SELECT * FROM INFORMATION_SCHEMA.COLUMNS")
+
+This will break up the information schema into bite-sized chunks that can be referenced by the LLM
+plan = vn.get_training_plan_generic(df_information_schema)
+
+If you like the plan, then uncomment this and run it to train
+vn.train(plan=plan)
+
+
+# **Demo**
+
+Please refer to infer.py, which trains RAG based on the train set of the bird dataset, with california_schools from the dev set used as the test set, and chromaDB+GPT4 as the backend.
+
+# **DataBase**
+
+## **Spider**
+Please refer to  [Spider](https://yale-lily.github.io/spider) for information related to Spyider database.
+
+## **Bird**
+Please refer to  [Bird](https://bird-bench.github.io)  for information related to Bird database.
---
+++ infer.py
@@ -0,0 +1,96 @@
+import vanna
+import os
+from vanna.openai import OpenAI_Chat
+from vanna.chromadb import ChromaDB_VectorStore
+from vanna.flask import VannaFlaskApp
+import json
+
+
+
+os.environ["TOKENIZERS_PARALLELISM"] = "false"
+
+class MyVanna(ChromaDB_VectorStore, OpenAI_Chat):
+    def __init__(self, config=None):
+        ChromaDB_VectorStore.__init__(self, config=config)
+        OpenAI_Chat.__init__(self, config=config)
+
+
+
+def train_ddl(vn):
+    df_ddl = vn.run_sql("SELECT type, sql FROM sqlite_master WHERE sql is not null")
+
+    for ddl in df_ddl['sql'].to_list():
+        vn.train(ddl=ddl)
+
+
+def remove_trained_data(vn):
+    training_data = vn.get_training_data()
+    ids = training_data['id']
+    for id_ in ids:
+        vn.remove_training_data(id=id_)
+
+def remove_specific_trained_data(vn):
+    training_data = vn.get_training_data()
+    for index, row in training_data.iterrows():
+        training_data_type = row['training_data_type']
+        if training_data_type != 'sql':
+            vn.remove_training_data(id=row['id'])
+
+
+
+def train_model(vn):
+    train_json_path = 'data/train_data/bird/train.json'
+    f1 = open(train_json_path, 'r')
+    json_list = json.load(f1)
+    f1.close()
+
+    length = len(json_list)
+    for i,pairs in enumerate(json_list):
+        #documentation = "database:{}; ".format(pairs['db_id'])+pairs["evidence"]
+        vn.train(question=pairs["question"], sql=pairs["SQL"])
+        print("start training model {}/{} data".format(i, length))
+    
+
+
+def train_document(vn):
+    dev_json_path = 'data/test_data/dev.json'
+    f1 = open(dev_json_path, 'r')
+    json_list = json.load(f1)
+    f1.close()
+    db_name = 'california_schools'
+    length = len(json_list)
+    for i,pairs in enumerate(json_list):
+        db_id = pairs['db_id']
+        if db_id == db_name:
+            evidences = pairs["evidence"].split(';')
+            if len(evidences) == 0 :
+                continue
+            else:
+                for evidence in evidences:
+                    vn.train(documentation = evidence)
+                    print("start training document {} data".format(i))
+
+
+
+
+def run():
+
+    vn = MyVanna(config={
+                'api_key' : 'eyJhbGciOiJIUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICI3Mzc5ZDJkOC00MTBlLTRhMjMtOWQzNS1iYTE5Y2RmMGRjNzIifQ.eyJpYXQiOjE3MTYyNzMzNTQsImp0aSI6ImMyMGM0M2EyLThkM2YtNDc5NC04ZGExLTM2Y2U3MjJmYzkyZCIsImlzcyI6Imh0dHA6Ly9iai5wcml2YXRlLmdsdW9uLW1lc29uLnRlY2g6ODAwNi9yZWFsbXMvZ2x1b24tbWVzb24iLCJhdWQiOiJodHRwOi8vYmoucHJpdmF0ZS5nbHVvbi1tZXNvbi50ZWNoOjgwMDYvcmVhbG1zL2dsdW9uLW1lc29uIiwic3ViIjoiNDhiOWFjM2MtODVhNy00YTUxLWI1MjMtMDU2NTUyNTBiZDg0IiwidHlwIjoiT2ZmbGluZSIsImF6cCI6ImNvbnNvbGUtdWkiLCJzZXNzaW9uX3N0YXRlIjoiZjgzYjc0ZTEtNGQ1ZS00MDU3LWE4ODUtNDI2YTYyYjZkMGJiIiwic2NvcGUiOiJwcm9maWxlIGVtYWlsIG9mZmxpbmVfYWNjZXNzIHJvbGVzIiwic2lkIjoiZjgzYjc0ZTEtNGQ1ZS00MDU3LWE4ODUtNDI2YTYyYjZkMGJiIn0.ZcH9UeEE6dJaXs8zsxdHUDbGMZQUdf5QMCbCFLlf90I',
+                'base_url' : 'http://bj.private.gluon-meson.tech:11000/model-proxy/v1',
+                'model' : 'gpt-4',
+                'path':'data/chromadb'
+                })
+    vn.connect_to_sqlite('./data/test_data/california_schools.sqlite')
+    #remove_trained_data(vn)
+    #train_ddl(vn)
+    #train_model(vn)
+    #train_document(vn)
+    suggested_db_name = 'california_schools'
+    suggested_db_path =  'data/test_data/dev.json'
+    app = VannaFlaskApp(vn, suggested_db_name=suggested_db_name, suggested_db_path=suggested_db_path)
+    host = '0.0.0.0'
+    port = '42210'
+    app.run(host=host, port=port)
+        
+run() 
